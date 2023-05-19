e65eef0326b20d6666a529abb3d01e81b57661f8
lalithadevivadde <122552921+lalithadevivadde@users.noreply.github.com>
2023-05-19 15:28:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,4 +23,4 @@ def update_news():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5002, timeout=120, workers=3, threads=3, worker-connections=1000)
+    app.run(host="0.0.0.0", port=5002, timeout=120, workers=3, threads=3)
